69af3bf320d8a64523446c08d0812517eb3a6d89
Andru Baskara Putra <andru,putra@binus.ac.id>
2021-10-01 23:41:02 +0700

    rest

--- api/rest.php
+++ api/rest.php
@@ -59,7 +59,7 @@ function regist_admin(){
     $check = array('id' => '','nama' => '','nik' => '','email' => '','katasandi' => '','c_sandi' => '','kk' => '','username' => '','alamat' => '','tempatlahir' => '','tanggallahir' => '','gender' => '','posisi' => '');
     $check_match = count(array_intersect_key($_post, $check));
     if ($check_match == count($check)) {
-        $result = mysqli_query($connect, "INSERT INTO karyawan SET
+        $result = mysqli_query($connect, "INSERT INTO admin SET
         id = '$_POST[id]',
         nama = '$_POST[nama]',
         nik = '$_POST[nik]',
@@ -123,6 +123,70 @@ function login_admin(){
         echo json_encode($response);
     }
 }
+
+function update_admin(){
+    global $koneksi;
+    if (!empty($_GET["id"])) {
+        $id = $_GET["id"];
+    }
+    $check = array('id' => '','nama' => '','nik' => '','email' => '','katasandi' => '','c_sandi' => '','kk' => '','username' => '','alamat' => '','tempatlahir' => '','tanggallahir' => '','gender' => '','posisi' => '');
+    $check_match = count(array_intersect_key($_POST, $check));
+    if ($check_match == count($check)) {
+        $result = mysqli_query($connect, "UPDATE admin SET               
+        id = '$_POST[id]',
+        nama = '$_POST[nama]',
+        nik = '$_POST[nik]',
+        email = '$_POST[email]',
+        katasandi = '$_POST[katasandi]',
+        c_sandi = '$_POST[c_sandi]',
+        username = '$_POST[username]',
+        alamat = '$_POST[alamat]',
+        tempatlahir = '$_POST[tempatlahir]',
+        tanggallahir = '$_POST[tanggallahir]',
+        gender = '$_POST[gender]',
+        posisi = '$_POST[posisi]' WHERE id = $id");
+
+        if ($result) {
+            $response=array(
+                'status' => 1,
+                'message' =>'Berhasil di update beb' 
+            );
+        }else{
+            $response=array(
+                'status' => 0,
+                'message' =>'yah.. gagal di update :('
+            ); 
+        }
+    }else{
+        $response=array(
+            'status' => 0,
+            'message' =>'parameternya salah gan',
+            'data' => $id
+        ); 
+    }
+}
+
+function hapus_admin(){
+    global $koneksi;
+    $id = $_GET['id'];
+    $query = "DELETE FROM admin WHERE id=".$id;
+
+    if (mysqli_query($koneksi, $query)) {
+        $response=array(
+            'status' => 1,
+            'message' =>'Berhasil di hapus wkwkw bye!'
+        ); 
+    }else{
+        $response=array(
+            'status' => 0,
+            'message' =>'gagal wkwkw, banyak2 doa dah lu jangan ateis mulu'
+        ); 
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
 
 
 
