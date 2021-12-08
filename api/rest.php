619b841066588e2859b4e8ab685b2f7e0f131330
HanustaviraGA <68433367+HanustaviraGA@users.noreply.github.com>
2021-12-08 22:58:25 +0700

    Rollback sistem hashing

--- api/rest.php
+++ api/rest.php
@@ -101,9 +101,6 @@ function register(){
     $cek_password = mysqli_real_escape_string($koneksi, $password);
     // Enkripsi
     // $hash_pw = hash('sha256', $cek_password);
-    $salt = 'sunibngalamgnalamsunibbinusmalangsunibmalangbinusngalam';
-    $rand_salt = rand(0, 255);
-    $hash_pw = hash('sha256', $rand_salt.$cek_password.$salt);
     
     // Unsecure Code - Periksa Duplikat
     // $cek_id = "SELECT * FROM penduduk WHERE no_kk = '$cek_no_kk' AND nik = '$cek_nik' AND nama = '$cek_nama' AND username = '$cek_username'";
@@ -128,7 +125,7 @@ function register(){
         $query = query($koneksi, 
             "INSERT INTO penduduk SET no_kk = ?, nik = ?, nama = ?, email = ?, username = ?, password = ?",
             'ssssss',
-            [$cek_no_kk, $cek_nik, $cek_nama, $cek_email, $cek_username, $hash_pw]
+            [$cek_no_kk, $cek_nik, $cek_nama, $cek_email, $cek_username, $cek_password]
         );
         if ($query) {
             $response = [
@@ -178,9 +175,6 @@ function login() {
         $password = $_POST['password'];
         $esc_username = mysqli_real_escape_string($koneksi, $username);
         $esc_password = mysqli_real_escape_string($koneksi, $password);
-        $salt = 'sunibngalamgnalamsunibbinusmalangsunibmalangbinusngalam';
-        $rand_salt = rand(0, 255);
-        $hash_pw = hash('sha256', $esc_password.$salt);
         // Unsecure code - Cek database
         // $query = "SELECT no_kk, nik, nama, tempat_lahir, tanggal_lahir, 
         // alamat, id_rt, id_rw, jenis_kelamin, agama, status_perkawinan, 
@@ -200,7 +194,7 @@ function login() {
         no_kitas, kepala_keluarga, nama_ayah, nama_ibu, virtual_account_id, foto_kk, 
         pendidikan, tanggal_pengeluaran_kk, tanggal_reg 
         FROM penduduk WHERE username = ? AND password = ?";
-        $data = query($koneksi, $query, 'ss', [$esc_username, $hash_pw]);
+        $data = query($koneksi, $query, 'ss', [$esc_username, $esc_password]);
         if($data){
             session_start();
             session_regenerate_id(true);
