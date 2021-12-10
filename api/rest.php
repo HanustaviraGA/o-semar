d6527ce0c536e1ec47afdf6cfa31585b8995da78
HanustaviraGA <68433367+HanustaviraGA@users.noreply.github.com>
2021-12-10 11:57:42 +0700

    Update lihat daftar surat dan laporan

--- api/rest.php
+++ api/rest.php
@@ -347,9 +347,9 @@ function lihat_daftar_surat(){
     if($data){
         $sql_surat = "SELECT suratketerangan.nik, suratketerangan.no_surat, 
         id_rt, id_rw, jenis, keperluan, tanggal_pengajuan, keterangan, status, 
-        alasan, lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran 
-        FROM lampiran INNER JOIN suratketerangan ON lampiran.nik = suratketerangan.nik  
-        WHERE lampiran.nik = ? AND jenis_lampiran = ?";
+        alasan, GROUP_CONCAT( DISTINCT lampiran ) AS lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran 
+        FROM suratketerangan INNER JOIN lampiran ON lampiran.kode = suratketerangan.no_surat  
+        WHERE suratketerangan.nik = ? AND jenis_lampiran = ? GROUP BY suratketerangan.no_surat";
         $data_surat = query($koneksi, $sql_surat, 'ss', [$esc_nik, 'Pengajuan Surat']);
         $response = generate_response(1, 'Sukses', $data_surat);
     }else{
@@ -448,9 +448,9 @@ function lihat_daftar_laporan(){
     if($data){
         $sql_pelaporan = "SELECT pelaporan.nik, pelaporan.id_pelaporan, 
         id_rt, id_rw, kategori, keterangan, tanggal_pelaporan, status, 
-        alasan, lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran 
-        FROM lampiran INNER JOIN pelaporan ON lampiran.nik = pelaporan.nik  
-        WHERE lampiran.nik = ? AND jenis_lampiran = ?";
+        alasan, GROUP_CONCAT( DISTINCT lampiran ) AS lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran 
+        FROM pelaporan INNER JOIN lampiran ON lampiran.kode = pelaporan.id_pelaporan  
+        WHERE pelaporan.nik = ? AND jenis_lampiran = ? GROUP BY pelaporan.id_pelaporan";
         $data_pelaporan = query($koneksi, $sql_pelaporan, 'ss', [$esc_nik, 'Laporan Masyarakat']);
         $response = generate_response(1, 'Sukses', $data_pelaporan);
     }else{
@@ -471,12 +471,12 @@ function lihat_iuran(){
     $esc_nik = mysqli_real_escape_string($koneksi, $nik);
     $esc_filter = mysqli_real_escape_string($koneksi, $filter);
     /*
-    Jenis status : 0 - Unpaid, 1 - Paid, 2 - Late
+    Jenis status : 0 - Unpaid, 1 - Paid
     */    
     // Unpaid
     if($esc_filter == 0){
         $sql = "SELECT * FROM tagihan WHERE nik = ? AND status_pembayaran = ?";
-        $data = query($koneksi, $sql, 'ss', [$esc_nik, 'Pending']);
+        $data = query($koneksi, $sql, 'ss', [$esc_nik, 'Unpaid']);
         if($data){
             $response = generate_response(1, 'Sukses', $data);
         }else{
@@ -486,17 +486,7 @@ function lihat_iuran(){
     // Paid
     else if($esc_filter == 1){
         $sql = "SELECT * FROM tagihan WHERE nik = ? AND status_pembayaran = ?";
-        $data = query($koneksi, $sql, 'ss', [$esc_nik, 'Lunas']);
-        if($data){
-            $response = generate_response(1, 'Sukses', $data);
-        }else{
-            $response = generate_response(0, 'Tidak ada Data');
-        }
-    }
-    // Late
-    else if($esc_filter == 2){
-        $sql = "SELECT * FROM tagihan WHERE nik = ? AND status_pembayaran = ?";
-        $data = query($koneksi, $sql, 'ss', [$esc_nik, 'Telat']);
+        $data = query($koneksi, $sql, 'ss', [$esc_nik, 'Paid']);
         if($data){
             $response = generate_response(1, 'Sukses', $data);
         }else{
@@ -516,15 +506,66 @@ function update_iuran(){
     global $koneksi;
     $nik = $_POST['nik'];
     $id_tagihan = $_POST['id_tagihan'];
+    $tanggal = date("Y-m-d");
+    // Escape
     $esc_nik = mysqli_real_escape_string($koneksi, $nik);
     $esc_id = mysqli_real_escape_string($koneksi, $id_tagihan);
     // Cek keberadaan data
     $sql = "SELECT * FROM tagihan WHERE nik = ? AND id_tagihan = ?";
     $data = query($koneksi, $sql, 'ss', [$esc_nik, $esc_id]);
     if($data){
-        $sql_upd = "UPDATE tagihan SET status_pembayaran = ? WHERE id_tagihan = ?";
-        $data = query($koneksi, $sql_upd, 'ss', ['Lunas', $esc_id]);
-        $response = generate_response(1, 'Sukses');
+        // Masukkan tabel di DB
+        if(isset($_FILES["files"]) && !empty($_FILES["files"]["name"])){
+            foreach($_FILES['files']['tmp_name'] as $key => $tmp_name ){
+                $file_name = $key.$_FILES['files']['name'][$key];
+                $file_size =$_FILES['files']['size'][$key];
+                $file_tmp =$_FILES['files']['tmp_name'][$key];
+                $file_type=$_FILES['files']['type'][$key];
+                
+                $original_filename = $_FILES['files']['name'][$key];
+                $ext = strtolower(pathinfo($_FILES["files"]["name"][$key], PATHINFO_EXTENSION));
+                // check extension and upload
+                if(in_array( $ext, array('jpg', 'jpeg', 'png', 'gif', 'bmp'))) {
+                    $filename_without_ext = basename($original_filename, '.'.$ext);
+                    $new_filename = uniqid() .  '_' . $nik . '.' . $ext;
+                    move_uploaded_file($file_tmp,'../admin/iuran/berkas/'.$new_filename);
+                    // Unsecure Code - Masuk Lampiran
+                    // $sql = "INSERT INTO lampiran(nik, kode, lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran) 
+                    // VALUES('$nik', '$uniqid', '$new_filename', 'Laporan Masyarakat', '$tanggal', 'Pending', '-')";
+                    // $query = mysqli_query($koneksi, $sql);
+
+                    // Masuk Lampiran
+                    $sql = "INSERT INTO lampiran(nik, kode, lampiran, jenis_lampiran, tanggal_lampiran, status_lampiran, ket_lampiran) 
+                    VALUES(?, ?, ?, 'Pembayaran Tagihan', ?, 'Paid', '-')";
+                    $query = query($koneksi, $sql, 'ssss', [$nik, $id_tagihan, $new_filename, $tanggal]);
+                    // Unsecure Code - Masuk Pelaporan
+                    // $sql_pelaporan = "INSERT INTO pelaporan(id_pelaporan, nik, id_rt, id_rw, kategori, keterangan, tanggal_pelaporan, status)
+                    // VALUES ('$uniqid', '$nik', '$rt', '$rw', '$kategori', '$keterangan', '$tanggal', 'Pending')";
+                    // $query_pelaporan = mysqli_query($koneksi, $sql_pelaporan);
+
+                    $sql_upd = "UPDATE tagihan SET status_pembayaran = ?, bukti_pembayaran = ? WHERE id_tagihan = ?";
+                    $data = query($koneksi, $sql_upd, 'sss', ['Lunas', $new_filename, $esc_id]);
+                    $response = generate_response(1, 'Sukses');
+                }
+                else{
+                    $response = generate_response(2, 'Ekstensi file tidak dapat diterima');
+                }
+            }
+        } else {
+            // Unsecure Code - Masuk Surat Keterangan
+            // $sql_pelaporan = "INSERT INTO pelaporan(id_pelaporan, nik, id_rt, id_rw, kategori, keterangan, tanggal_pelaporan, status)
+            // VALUES ('$uniqid', '$nik', '$rt', '$rw', '$kategori', '$keterangan', '$tanggal', 'Pending')";
+            // $query_pelaporan = mysqli_query($koneksi, $sql_pelaporan);
+
+            // Masuk Surat Keterangan
+            $sql_upd = "UPDATE tagihan SET status_pembayaran = ? WHERE id_tagihan = ?";
+            $data = query($koneksi, $sql_upd, 'ss', ['Lunas', $esc_id]);
+            if($data){
+                $response = generate_response(1, 'Sukses');
+            }else{
+                $response = generate_response(0, 'Gagal');
+            }
+        }
     }else{
         $response = generate_response(0, 'Tidak ada Data');
     }
